6b538dac7037ad49496aaacfde5a6b7f8c1a27f7
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-06-15 22:46:12 +0800

    基础环境

--- src/main/java/com/smd/loadingscreens/LoadingScreens.java
+++ src/main/java/com/smd/loadingscreens/LoadingScreens.java
@@ -1,4 +1,4 @@
-package com.example.modid;
+package com.smd.loadingscreens;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -6,15 +6,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class LoadingScreens {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
-    /**
-     * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
-     *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
-     * </a>
-     */
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
